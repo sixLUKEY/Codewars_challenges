8bf8e180ac38470cd35678a80572a601748b363a
sixLUKEY <marcoslukerojo@gmail.com>
2024-03-20 15:18:22 +0200

    First test setup with Mocha and Chai

---
+++ Typescript/Are_you_playing_banjo/app.ts
@@ -0,0 +1,7 @@
+export function areYouPlayingBanjo(name: string): string {
+    let workingName = name.toLowerCase().trim();
+    if (workingName.startsWith("r")) {
+      return `${name} plays banjo`;
+    }
+    return `${name} does not play banjo`;
+  }
---
+++ test/app.spec.ts
@@ -0,0 +1,15 @@
+import { assert } from "chai";
+import { areYouPlayingBanjo } from "../app";
+
+describe("Are you playing banjo?", () => {
+  it("Fixed test", () => {
+    assert.strictEqual(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+    assert.strictEqual(
+      areYouPlayingBanjo("bravo"),
+      "bravo does not play banjo"
+    );
+    assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
+  });
+});
